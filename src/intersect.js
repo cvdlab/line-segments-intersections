3f1b99c3125c257ca866ea1ca4c8efc70ac303cf
Christian Vadalà <chr.vadala@gmail.com>
2016-05-11 18:00:20 +0200

    change start/end vertices with v0/v1

--- src/intersect.js
+++ src/intersect.js
@@ -3,8 +3,8 @@ var lineIntersect = require('line-intersect');
 let intersect = function (line1, line2) {
 
   var result = lineIntersect.checkIntersection(
-    line1.start.x, line1.start.y, line1.end.x, line1.end.y,
-    line2.start.x, line2.start.y, line2.end.x, line2.end.y
+    line1.v0.x, line1.v0.y, line1.v1.x, line1.v1.y,
+    line2.v0.x, line2.v0.y, line2.v1.x, line2.v1.y
   );
 
   if (result.type === 'intersecting')
--- test/test-intersect.js
+++ test/test-intersect.js
@@ -5,8 +5,8 @@ let assert = require('assert');
 describe('intersections', function () {
   it('should return intersection', function () {
 
-    var line1 = {start: {x: 10, y: 10}, end: {x: 0, y: 0}};
-    var line2 = {start: {x: 10, y: 0}, end: {x: 0, y: 10}};
+    var line1 = {v0: {x: 10, y: 10}, v1: {x: 0, y: 0}};
+    var line2 = {v0: {x: 10, y: 0}, v1: {x: 0, y: 10}};
     var expected = {x: 5, y:5};
 
     let result = intersect(line1, line2);
