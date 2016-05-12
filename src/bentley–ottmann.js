834b29e6c32b9d1f917b7fd349f46ea8cc685d36
Christian Vadalà <chr.vadala@gmail.com>
2016-05-12 15:04:50 +0200

    adds events-queue

--- src/bentley–ottmann.js
+++ src/bentley–ottmann.js
@@ -1,10 +1,12 @@
-var intersect = require('./intersect');
+let intersect = require('./intersect');
 let LinesList = require('./lines-list');
+let EventsQueue = require('./events-queue');
 
 
 const A = {v0: {x: 10, y: 10}, v1: {x: 0, y: 0}};
 const B = {v0: {x: 10, y: 0}, v1: {x: 0, y: 10}};
 const C = {v0: {x: 5, y: 3}, v1: {x: 10, y: 7}};
+const D = {v0: {x: 15, y: 3}, v1: {x: 10, y: 7}};
 
 let verticesComparator = (a, b) => {
   if (a.x === b.x) {
@@ -39,17 +41,31 @@ let linesComparator = (a, b) => {
 
 
 
-var sl = new LinesList(linesComparator);
-sl.add(A);
-sl.add(B);
-sl.add(C);
+//var sl = new LinesList(linesComparator);
+//sl.add(A);
+//sl.add(B);
+//sl.add(C);
+//
+//console.log(sl.lines);
+//
+//sl.swap(A, C);
+//
+//console.log(sl.lines);
 
-console.log(sl.lines);
 
-sl.swap(A, C);
 
-console.log(sl.lines);
+//var eq = new EventsQueue();
+//
+//eq.enqueueAddEvent(C);
+//eq.enqueueAddEvent(B);
+//console.log(eq.next());
+//eq.enqueueAddEvent(A);
+//console.log(eq.next());
+//console.log(eq.next());
+//console.log(eq.next());
 
+
+//console.log(eq.events);
 
 
 
---
+++ src/events-queue.js
@@ -0,0 +1,67 @@
+class EventsQueue {
+  constructor() {
+    this.events = [];
+    this.current = 0;
+  }
+
+  enqueueAddEvent(line) {
+    return this.enqueueEvent({
+      type: EventsQueue.ADD,
+      line,
+      vertex: line.v0
+    });
+  }
+
+  enqueueRemoveEvent(line) {
+    return this.enqueueEvent({
+      type: EventsQueue.REMOVE,
+      line,
+      vertex: line.v1
+    });
+  }
+
+  enqueueSwapEvent(lineA, lineB, intersection) {
+    return this.enqueueEvent({
+      type: EventsQueue.SWAP,
+      lineA,
+      lineB,
+      vertex: intersection
+    });
+  }
+
+  next() {
+    let event = this.events[this.current];
+    this.current++;
+    return event || false;
+  }
+
+  reset() {
+    this.current = 0;
+  }
+
+  enqueueEvent(event) {
+    let events = this.events;
+    let vertex = event.vertex;
+    let pos = 0;
+
+    let stopCondition = (pos) => {
+      let curVertex = events[pos].vertex;
+      return curVertex.x < vertex.x || curVertex.x === vertex.x && curVertex.y < vertex.y
+    };
+
+    while (pos < events.length && stopCondition(pos)) {
+      pos++;
+    }
+
+    console.log(pos);
+
+    events.splice(pos, 0, event);
+    return pos;
+  }
+}
+
+EventsQueue.ADD = 'ADD';
+EventsQueue.REMOVE = 'REMOVE';
+EventsQueue.SWAP = 'SWAP';
+
+module.exports = EventsQueue;
