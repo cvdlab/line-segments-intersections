6ba6f6c71b2eb2d06c25da6270ebc484091fff95
Christian Vadalà <chr.vadala@gmail.com>
2016-05-16 18:40:12 +0200

    adds test lines

--- run.js
+++ run.js
@@ -2,10 +2,10 @@ let bentleyOttmann = require('./src/bentley–ottmann');
 let lines2svg = require('./src/lines2svg');
 let path = require('path');
 
-const A = {v0: {x: 10, y: 10}, v1: {x: 0, y: 0}, id: 'A'};
-const B = {v0: {x: 10, y: 0}, v1: {x: 0, y: 10}, id: 'B'};
-const C = {v0: {x: 5, y: 3}, v1: {x: 10, y: 7}, id: 'C'};
-const D = {v0: {x: 15, y: 3}, v1: {x: 10, y: 7}, id: 'D'};
+//const A = {v0: {x: 10, y: 10}, v1: {x: 0, y: 0}, id: 'A'};
+//const B = {v0: {x: 10, y: 0}, v1: {x: 0, y: 10}, id: 'B'};
+//const C = {v0: {x: 5, y: 3}, v1: {x: 10, y: 7}, id: 'C'};
+//const D = {v0: {x: 15, y: 3}, v1: {x: 10, y: 7}, id: 'D'};
 
 
 //const E = {v0: {x: 0, y: 0}, v1: {x: 10, y: 10}, id: 'E'};
@@ -25,8 +25,22 @@ const G = {id: 3, v0: {x: 383.56205801744915, y: 328.9974359034239}, v1: {x: 452
 const H = {id: 4, v0:{x:108.92707603727425, y:95.13064569196628},v1:{x:241.7521642842455, y:497.9883960035628}};
 const I = {id: 5, v0: {x:126.01327686688235, y:219.23409454477795},v1:{x:187.47404399932944, y:194.6360931908001}};
 
+//const A = {id: 1, v0: {x: 63, y: 397}, v1: {x: 955, y: 142}};
+//const B = {id: 2, v0: {x: 148, y: 224}, v1: {x: 490, y: 371}};
+//const C = {id: 3, v0: {x: 193, y: 285}, v1: {x: 444, y: 303}};
+//const D = {id: 4, v0: {x: 103, y: 175}, v1: {x: 276, y: 132}};
+//const E = {id: 5, v0: {x: 236, y: 85}, v1: {x: 874, y: 248}};
+//const F = {id: 6, v0: {x: 558, y: 183}, v1: {x: 627, y: 122}};
 
-let lines = [  E, F, G, H, I];
+
+
+//const A = {id: 1, v0: {x: 0, y: 100}, v1: {x: 150, y: 10}};
+//const B = {id: 1, v0: {x: 50, y: 50}, v1: {x: 200, y: 60}};
+//const C = {id: 1, v0: {x: 150, y: 30}, v1: {x: 210, y: 80}};
+
+
+
+let lines = [E, F, G, H, I];
 let intersections = bentleyOttmann(lines);
 
 console.log('bentley-ottman', intersections);
