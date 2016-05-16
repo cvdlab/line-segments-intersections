deb565cb0f76ce9cba02737e084e69f753ee15e6
Christian Vadalà <chr.vadala@gmail.com>
2016-05-16 16:50:45 +0200

    change y axis direction

--- src/lines2svg.js
+++ src/lines2svg.js
@@ -2,10 +2,20 @@ let fs = require('fs');
 
 module.exports = function lines2svg(lines, intersections, filename) {
 
+  let width = Number.NEGATIVE_INFINITY;
+  let height = Number.NEGATIVE_INFINITY;
+
+  lines.forEach(line => {
+    width = Math.max(width, line.v0.x, line.v1.x);
+    height = Math.max(height, line.v0.y, line.v1.y);
+  });
+
   let fd = fs.openSync(filename, 'w');
 
   fs.writeSync(fd, '<?xml version="1.0" standalone="yes"?>\n');
-  fs.writeSync(fd, '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" height="10000" width="10000" >\n');
+  fs.writeSync(fd, '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" height="' + height + '" width="' + width + '" >\n');
+  fs.writeSync(fd, '<g transform="translate(0, ' + height + ')" >\n');
+  fs.writeSync(fd, '<g transform="scale(1,-1)" >\n');
 
   lines.forEach(segment => {
     fs.writeSync(fd, '<line x1="' + segment.v0.x + '" y1="' + segment.v0.y + '" x2="' + segment.v1.x + '" y2="' + segment.v1.y + '" stroke-width="1" stroke="#ddd" />\n');
@@ -15,6 +25,8 @@ module.exports = function lines2svg(lines, intersections, filename) {
     fs.writeSync(fd, '<circle r="2" cx="' + intersection.vertex.x + '" cy="' + intersection.vertex.y + '" fill="red" />\n');
   });
 
+  fs.writeSync(fd, "</g>");
+  fs.writeSync(fd, "</g>");
   fs.writeSync(fd, "</svg>");
   fs.closeSync(fd);
 };
